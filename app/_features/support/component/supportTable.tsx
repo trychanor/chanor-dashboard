8650ad8617b4628b6b753c809bab3b77db8a2ff4
Nweke Cynthia Chinenye <nwekecynthia2015@gmail.com>
2025-12-13 00:06:12 -0800

    icon fixes on the support page

--- app/_features/support/component/supportTable.tsx
+++ app/_features/support/component/supportTable.tsx
@@ -1,6 +1,6 @@
 import Table from "@/app/_ui/Table";
-import { CgCalendarDates } from "react-icons/cg";
-import { MdOutlineRefresh } from "react-icons/md";
+import { CalendarDays } from "lucide-react";
+import { RotateCw } from "lucide-react";
 import Status from "../../../_ui/Status";
 
 export default function SupportTable() {
@@ -50,14 +50,14 @@ export default function SupportTable() {
       {/* calendar and date */}
       <div className="flex justify-end items-center gap-[36px] p-6">
         <div className="flex justify-center h-[44px] px-2 rounded-[8px] items-center gap-[21px] border-[1px] border-[#C7C7CC] text-[#667085]">
-          <CgCalendarDates size={20} />
+          <CalendarDays size={20} />
           <h2 className="text-[12px] md:text-[14px] font-[500] leading-[100%]">
             May 6, 2025 - 11:32 AM
           </h2>
         </div>
         {/* refresh */}
         <div className="flex gap-[10px] text-[#0070FF] items-center">
-          <MdOutlineRefresh size={20} />
+          <RotateCw size={20} />
           <h3 className="text-[14px] font-[500] leading-[100%]">Refresh </h3>
         </div>
       </div>
